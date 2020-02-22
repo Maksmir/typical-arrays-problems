4c192a693d62a4693202d4f005d8d56b86fdfb98
Maksmir <39415242+Maksmir@users.noreply.github.com>
2020-02-22 09:38:48 +0300

    feat: completed the typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 
-exports.min = function min (array) {
-  let minN = array[0]
-  for(let i=1; i<array.length; i++){
-    if (minN > array[i]){
-      minN = array[i]
+exports.min = function min(array) {
+  let minN = 0
+  if (array && array.length>0) {
+    minN = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (minN > array[i]) {
+        minN = array[i]
+      }
     }
   }
   return minN
 }
 
-exports.max = function max (array) {
-  let maxN = array[0]
-  for(let i=1; i<array.length; i++){
-    if (maxN < array[i]){
-      maxN = array[i]
+exports.max = function max(array) {
+  let maxN = 0
+  if (array && array.length>0) {
+    maxN = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (maxN < array[i]) {
+        maxN = array[i]
+      }
     }
   }
   return maxN
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
   let mean = 0
-  for(let i=0; i<array.length; i++){
-    mean+=array[i]
+  if (array && array.length>0) {
+    for (let i = 0; i < array.length; i++) {
+      mean += array[i]
+    }
+    mean /= array.length
   }
-  mean/=array.length
   return mean;
 }
